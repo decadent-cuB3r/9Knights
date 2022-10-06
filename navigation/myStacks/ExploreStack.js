ccbeb37125bdfbd25ef28df46060183ab7fc85a9
decadent-cuB3r <cub3r.ezra.yu@gmail.com>
2022-10-06 16:23:13 +0800

    change computer

--- navigation/myStacks/ExploreStack.js
+++ navigation/myStacks/ExploreStack.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-
+import Icons from 'react-native-vector-icons/Ionicons';
+import { useFonts } from 'expo-font';
 //Explore Stack Components
 import HomeScreen from '../../screens/HomeScreen';
 
 //Theme and color mode
 import { useTheme } from '@react-navigation/native'
+
+// const [fontsLoaded] = useFonts({
+//     'Inter-Black': require('./assets/fonts/NotoSerifTC-Bold.otf'),
+//   });
 
 //stack initial
 const Stack = createStackNavigator();
@@ -16,6 +21,24 @@ const ExploreStack = () => {
             <Stack.Screen
                 name="HomeScreen"
                 component={HomeScreen}
+                options={({ navigation }) => ({
+                    title: '探索旅程',
+                    headerTitleAlign: 'left', 
+                    headerStyle: {
+                        backgroundColor: '#DA4F40',
+                      },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontSize: 32,
+                        fontFamily: 'Noto Serif TC'
+                    },
+                headerRight: () => (
+                    <Icons></Icons>
+                ),
+                headerRight: () => (
+                    <Icons></Icons>
+                ),
+                })}
             />
         </Stack.Navigator>
     )
